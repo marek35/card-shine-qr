6d03cfd45c7ee94edfdd5c508916ada47aac1c54
marek35 <59792980+marek35@users.noreply.github.com>
2026-08-31 14:09:45 +0200

    Remove configuration from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,3 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  vite: {
-    base: '/card-shine-qr/',
-  },
-  tanstackStart: {
-    server: { entry: "server" },
-  },
-});
+export default defineConfig({});
